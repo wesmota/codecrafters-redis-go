04f14e775f9c4cf58f877ec0347bcfad42af1381
Wesley Mota Araujo <wesley.araujo@varsitytutors.com>
2022-12-15 12:03:41 -0300

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -14,10 +14,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
 	conn.Write([]byte("+PONG\r\n"))
 }
